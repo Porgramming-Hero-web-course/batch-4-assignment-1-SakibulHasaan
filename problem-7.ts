7f8d91cdc14a4798b70dec07a1bf6977f8e08c8a
Md Sakibul Hasan <md.sakibul.hasan@b-architects.com>
2024-11-08 21:07:16 +0900

    added problem 7 solution

---
+++ problem-7.ts
@@ -0,0 +1,17 @@
+// Problem 7
+
+class Car {
+  make: string;
+  model: string;
+  year: number;
+
+  constructor(make: string, model: string, year: number) {
+    this.make = make;
+    this.model = model;
+    this.year = year;
+  }
+
+  getCarAge(): number {
+    return new Date().getFullYear() - this.year;
+  }
+}
